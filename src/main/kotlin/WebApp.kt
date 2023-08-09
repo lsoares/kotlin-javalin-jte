3df399bd3437ab530c57ab4c7459c6fdafb2b8f3
Luís Soares <lsoares@newstore.com>
2023-08-09 20:08:21 +0100

    use an if

--- src/main/kotlin/WebApp.kt
+++ src/main/kotlin/WebApp.kt
@@ -1,9 +1,14 @@
 import io.javalin.Javalin
 import io.javalin.rendering.template.JavalinJte
+import kotlin.random.Random
 
 fun main() {
     JavalinJte.init()
     Javalin.create()
-        .get("/") { it.render("hello.jte") }
+        .get("/") {
+            it.render(
+                "hello.jte", mapOf("guess" to Random.nextInt(21))
+            )
+        }
         .start()
 }
